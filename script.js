d5b32940a9c53006c6bb048b434edeb7929cda28
NguyenDuyTien <tiennguyenduy99123@gmail.com>
2025-03-07 17:04:30 +0700

    update progress bar

--- script.js
+++ script.js
@@ -12,29 +12,31 @@ input.addEventListener("keydown", (e) => {
 			item.classList.toggle("done");
 			let total_tasks = list.querySelectorAll("li").length;
 			let completed_tasks = list.querySelectorAll("li.done").length;
-			if (total_tasks == 0 && completed_tasks == total_tasks) {
+			if (total_tasks == 0 || completed_tasks == 0) {
 				document.querySelector(".progress__bar").style.width = "0%";
-				document.querySelector(".progress__bar").style.background = "rgb(255, 61, 61)";
-				document.querySelector(".progress__bar").style.border = "1px solid rgb(255, 61, 61)";
 				percent.innerHTML = "0%";
+				document.querySelector(".progress__bar").style.background = "rgb(255, 61, 61)";
+				document.querySelector(".progress__bar .percent").style.border =
+					"1px solid rgb(255, 61, 61)";
 			} else {
 				let progress = Math.floor((completed_tasks / total_tasks) * 100);
-				if (progress < 35) {
+				if (progress < 35 && progress > 0) {
 					document.querySelector(".progress__bar").style.width = `${progress}%`;
-					document.querySelector(".progress__bar").style.background = "rgb(255, 61, 61)";
-					document.querySelector(".progress__bar span").style.border = "1px solid rgb(255, 61, 61)";
 					percent.innerHTML = `${progress}%`;
+					document.querySelector(".progress__bar").style.background = "rgb(255, 61, 61)";
+					document.querySelector(".progress__bar .percent").style.border =
+						"1px solid rgb(255, 61, 61)";
 				} else if (progress >= 35 && progress < 70) {
 					document.querySelector(".progress__bar").style.width = `${progress}%`;
-					document.querySelector(".progress__bar").style.background = "rgb(209, 214, 70)";
-					document.querySelector(".progress__bar span").style.border =
-						"1px solid rgb(209, 214, 70)";
 					percent.innerHTML = `${progress}%`;
-				} else {
+					document.querySelector(".progress__bar").style.background = "rgb(209, 214, 70)";
+					document.querySelector(".progress__bar .percent").style.border =
+						"1px solid rgb(209, 214, 70)";
+				} else if (progress > 70) {
 					document.querySelector(".progress__bar").style.width = `${progress}%`;
 					percent.innerHTML = `${progress}%`;
 					document.querySelector(".progress__bar").style.background = "rgb(44, 211, 114)";
-					document.querySelector(".progress__bar span").style.border =
+					document.querySelector(".progress__bar .percent").style.border =
 						"1px solid  rgb(44, 211, 114)";
 				}
 			}
@@ -46,28 +48,32 @@ input.addEventListener("keydown", (e) => {
 
 		let total_tasks = list.querySelectorAll("li").length;
 		let completed_tasks = list.querySelectorAll("li.done").length;
-		if (total_tasks == 0 && completed_tasks == total_tasks) {
+		if (completed_tasks == 0 || total_tasks == 0) {
 			document.querySelector(".progress__bar").style.width = "0%";
-			document.querySelector(".progress__bar").style.background = "rgb(255, 61, 61)";
-			document.querySelector(".progress__bar").style.border = "1px solid redrgb(255, 61, 61)";
 			percent.innerHTML = "0%";
+			document.querySelector(".progress__bar").style.background = "rgb(255, 61, 61)";
+			document.querySelector(".progress__bar .percent").style.border =
+				"1px solid red rgb(255, 61, 61)";
 		} else {
 			let progress = Math.floor((completed_tasks / total_tasks) * 100);
-			if (progress < 35) {
+			if (progress < 35 && progress > 0) {
 				document.querySelector(".progress__bar").style.width = `${progress}%`;
-				document.querySelector(".progress__bar").style.background = "rgb(255, 61, 61)";
-				document.querySelector(".progress__bar span").style.border = "1px solid rgb(255, 61, 61)";
 				percent.innerHTML = `${progress}%`;
+				document.querySelector(".progress__bar").style.background = "rgb(255, 61, 61)";
+				document.querySelector(".progress__bar .percent").style.border =
+					"1px solid rgb(255, 61, 61)";
 			} else if (progress >= 35 && progress < 70) {
 				document.querySelector(".progress__bar").style.width = `${progress}%`;
+				percent.innerHTML = `${progress}%`;
 				document.querySelector(".progress__bar").style.background = "rgb(209, 214, 70)";
-				document.querySelector(".progress__bar span").style.border = "1px solid rgb(209, 214, 70)";
-				percent.innerHTML = `${progress}%`;
-			} else {
+				document.querySelector(".progress__bar .percent").style.border =
+					percent.innerHTML = `${progress}%`;
+				("1px solid rgb(209, 214, 70)");
+			} else if (progress > 70) {
 				document.querySelector(".progress__bar").style.width = `${progress}%`;
-				percent.innerHTML = `${progress}%`;
 				document.querySelector(".progress__bar").style.background = " rgb(44, 211, 114)";
-				document.querySelector(".progress__bar span").style.border = "1px solid  rgb(44, 211, 114)";
+				document.querySelector(".progress__bar .percent").style.border =
+					"1px solid  rgb(44, 211, 114)";
 			}
 		}
 
